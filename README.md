d2ec74f9c5e16783a3f65ab3a549388c71f64ad0
marcelo <madeinhellh@gmail.com>
2021-06-15 02:56:50 -0800

    Project Section1

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ Setup  y creacion de  Navbar
 # Hero Sections
 
 creacion de botones y texto de la seccion de Hero  y descomentar partes de  la archivo index.js  
+
+# Project Section
+
+Se crea la seccion donde van las ventanas para los proyectos y demas cosas que lo acompañan.
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,10 +4,36 @@ import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag,
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
+
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({ id, image, title, description, tags, source, visit}) => (
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) =>(
+                <Tag Key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
+          <UtilityList>
+            <ExternalLinks herf={visit}>Code</ExternalLinks>
+            <ExternalLinks herf={source}>Source</ExternalLinks>
+          </UtilityList>
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
